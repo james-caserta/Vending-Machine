0dc9a621704c0b851c8158d525838dcde65dc369
James Caserta <jcaserta90@gmail.com>
2021-07-23 15:51:54 -0400

    records addition

--- java/src/main/java/com/techelevator/Records.java
+++ java/src/main/java/com/techelevator/Records.java
@@ -1,6 +1,38 @@
 package com.techelevator;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.PrintWriter;
+
 public class Records {
+
+    private File file;
+    public Records(String nameOfFile) {
+        this.file = new File(nameOfFile);
+    }
+    public void writeToFile(String lineOfText) {
+        PrintWriter printWriter = null;
+        if (this.file.exists()) {
+            try {
+                FileOutputStream fileOutputStream = new FileOutputStream(file, true);
+                printWriter = new PrintWriter(fileOutputStream);
+                printWriter.println(lineOfText);
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+        } else {
+            try {
+                printWriter = new PrintWriter(this.file);
+                printWriter.println("first line.");
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+        }
+        printWriter.flush();
+        printWriter.close();
+    }
+}
 
 
 }
